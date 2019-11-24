72ec11e07b5dc54055f0e07d1a75c060f4206be9
las vega <kossdd@gmail.com>
2019-11-25 01:24:19 +0200

    add canvas

--- html/task-3/script.js
+++ html/task-3/script.js
@@ -9,3 +9,16 @@ if ('content' in document.createElement('template')) {
 } else {
     console.log('templates are not supported');
 }
+
+var canvas = document.getElementById('c1');
+var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(100, 50, 150, 75);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(150, 100, 100, 50);
+
+    // ctx.fillRect(x, y, width, height);
+
+
+console.log(ctx);
