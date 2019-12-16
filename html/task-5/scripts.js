3e13e049fba24e13dd54760e1fc35553c610c067
las vega <kosssm155@gmail.com>
2019-12-16 02:11:16 +0200

    task-5 footer menu about done

--- html/task-5/scripts.js
+++ html/task-5/scripts.js
@@ -1,4 +1,4 @@
-//Create function to select elements
+// Create function to select elements
 const selectElement = (element) => document.querySelector(element);
 
 //Open and close nav on click
@@ -6,3 +6,10 @@ selectElement('.menu-icons').addEventListener('click', () => {
     selectElement('nav').classList.toggle('active');
 })
 
+let links = document.querySelectorAll('nav a');
+
+for(let i = 11; i <= 14; i++) {
+    links[i].addEventListener('click', function() {
+        selectElement('nav').classList.remove('active');
+    })
+}
