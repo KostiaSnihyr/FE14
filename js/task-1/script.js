a3d8401fe46d7947a7b8fe25cb03c03c3638d29c
las vega <kosssm155@gmail.com>
2019-12-17 15:50:01 +0200

    fib finish

--- js/task-1/script.js
+++ js/task-1/script.js
@@ -2,12 +2,22 @@ function fibo(num) {
     let arr = [0, 1];
     let res = 0;
 
-    while (arr.length < num) {
-        res = arr[arr.length - 1] + arr[arr.length - 2];
-        arr.push(res);
+    if(num === null) {
+        console.log('cancelled fibo')
+    } else if(num == 0 || num == 1) {
+        console.log(num);
+        fibo(prompt("what's your sign?"));
+    } else if(isNaN(num - 0)) {
+        console.log('type in number');
+        fibo(prompt("what's your sign?"));
+    } else {
+        while (arr.length < num) {
+            res = arr[arr.length - 1] + arr[arr.length - 2];
+            arr.push(res);
+        }
+        console.log(arr);
+        fibo(prompt("what's your sign?"));
     }
-
-    console.log(arr);
 }
 
 fibo(prompt("what's your sign?"));
